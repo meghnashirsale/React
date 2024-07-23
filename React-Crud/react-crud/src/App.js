d71b7519d26deb4653bea49dbceb25392982d617
meghnashirsale <meghnashirsale31@gmail.com>
2024-07-23 15:41:00 +0530

    Reacrjs Curd Operation
    Curd Using ReactJS

--- React-Crud/react-crud/src/App.js
+++ React-Crud/react-crud/src/App.js
@@ -4,19 +4,20 @@ import { EmployeeData } from "./EmployeeData";
 
 function App() {
   const [data, setData] = useState([]);
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [age, setAge] = useState(0);
   const [id, setId] = useState(0);
-  const [isUpdate, setIsUpdate]=useState(false)
+  const [isUpdate, setIsUpdate] = useState(false);
 
   useEffect(() => {
     setData(EmployeeData);
   }, []);
 
   const handleEdit = (id) => {
-    const item = data.find(item => item.id === id);
+    const item = data.find((item) => item.id === id);
     if (item) {
+      setIsUpdate(true);
       setId(id);
       setFirstName(item.firstName);
       setLastName(item.lastName);
@@ -33,46 +34,115 @@ function App() {
     }
   };
 
-  const handleSave = () => {
+  const handleSave = (e) => {
+    let error='';
+    if(firstName ==='')
+      error += 'First Name is required ,';
+    if(lastName ==='')
+      error += 'Last Name is required ,';
+    if(age <=0 )
+      error += 'Age is required';
 
-     
+    if(error ==='')
+
+{
+    e.preventDefault();
+    const dt = [...data];
+
+    const newObject={
+      id: EmployeeData.length+1,
+      firstName: firstName,
+      lastName: lastName,
+      age: age
+    }
+    dt.push(newObject);
+    setData(dt);
+  }
+  else{
+    alert(error);
+  }
   };
 
-  const handleUpdate=()=>{
+  const handleUpdate = () => {
+    const index = data
+      .map((item) => {
+        return item.id;
+      })
+      .indexOf(id);
+    const dt = [...data];
+    dt[index].firstName = firstName;
+    dt[index].lastName = lastName;
+    dt[index].age = age;
+    setData(dt);
+    handleClear();
+  };
 
-  }
   const handleClear = () => {
     setId(0);
-    setFirstName('');
-    setLastName('');
+    setFirstName("");
+    setLastName("");
     setAge(0);
+    setIsUpdate(false);
   };
 
   return (
     <div className="App">
-      <div style={{ display: "flex", justifyContent: "center", marginTop: "10px", marginBottom: "10px" }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          marginTop: "10px",
+          marginBottom: "10px",
+        }}
+      >
+
+
         <div>
           <label>
             First Name:
-            <input value={firstName} type="text" placeholder='Enter First Name' onChange={(e) => setFirstName(e.target.value)} />
+            <input
+              value={firstName}
+              type="text"
+              placeholder="Enter First Name"
+              onChange={(e) => setFirstName(e.target.value)}
+            />
           </label>
         </div>
         <div>
           <label>
             Last Name:
-            <input value={lastName} type="text" placeholder='Enter Last Name' onChange={(e) => setLastName(e.target.value)} />
+            <input
+              value={lastName}
+              type="text"
+              placeholder="Enter Last Name"
+              onChange={(e) => setLastName(e.target.value)}
+            />
           </label>
         </div>
         <div>
           <label>
             Age:
-            <input value={age} type="number" placeholder='Enter Age' onChange={(e) => setAge(e.target.value)} />
+            <input
+              value={age}
+              type="number"
+              placeholder="Enter Age"
+              onChange={(e) => setAge(e.target.value)}
+            />
           </label>
         </div>
         <div>
-          <button className="btn btn-primary" onClick={()=>handleSave()}>Save</button> &nbsp;
-          <button className="btn btn-primary" onClick={()=>handleUpdate()}>Update</button> &nbsp;
-          <button className="btn btn-danger" onClick={()=>handleClear()}>Clear</button>
+          {!isUpdate ? (
+            <button className="btn btn-primary" onClick={(e) => handleSave(e)}>
+              Save
+            </button>
+          ) : (
+            <button className="btn btn-primary" onClick={() => handleUpdate()}>
+              Update
+            </button>
+          )}
+          <button className="btn btn-danger" onClick={() => handleClear()}>
+            Clear
+          </button>
         </div>
       </div>
 
@@ -96,10 +166,17 @@ function App() {
               <td>{item.lastName}</td>
               <td>{item.age}</td>
               <td>
-                <button className="btn btn-primary" onClick={() => handleEdit(item.id)}>
+                <button
+                  className="btn btn-primary"
+                  onClick={() => handleEdit(item.id)}
+                >
                   Edit
-                </button> &nbsp;
-                <button className="btn btn-danger" onClick={() => handleDelete(item.id)}>
+                </button>{" "}
+                &nbsp;
+                <button
+                  className="btn btn-danger"
+                  onClick={() => handleDelete(item.id)}
+                >
                   Delete
                 </button>
               </td>
